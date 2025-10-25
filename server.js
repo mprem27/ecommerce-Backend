c244074e0ae6444b4d5ec4287a56793bfddcabf8
mprem27 <malepatipremkumarreddy@gmail.com>
2025-10-25 18:06:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,24 +14,26 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors({
-  origin:[ 'https://ecommerce-frontend-two-beige.vercel.app', 'https://ecommerce-admin-ten-psi.vercel.app']
+  origin: [
+    'https://ecommerce-frontend-two-beige.vercel.app', 
+    'https://ecommerce-admin-ten-psi.vercel.app'
+  ],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
 }));
 
 connectDB();
 connectClodinary();
 
-app.use('/api/user',UserRouter);
-app.use('/api/Product',ProductRouter);
-app.use('/api/cart',cartRouter);
-app.use('/api/order',orderRouter);
+app.use('/api/user', UserRouter);
+app.use('/api/Product', ProductRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/order', orderRouter);
 
 app.get('/', (req,res)=>{
     res.send('API IS WORKING....');
 });
 
-app.listen(port,()=>{
-    console.log('server is running :'+port);
+app.listen(port, ()=>{
+    console.log('server is running :' + port);
 });
-
