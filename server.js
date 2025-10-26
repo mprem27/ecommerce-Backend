4cb40f37cefc4dbb226602b8835510d0a8f587fa
mprem27 <malepatipremkumarreddy@gmail.com>
2025-10-26 08:50:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'https://ecommerce-frontend-two-beige.vercel.app', 
-    'https://ecommerce-admin-ten-psi.vercel.app'
+    'https://ecommerce-admin-ten-psi.vercel.app',
+    'http://localhost:5173'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
